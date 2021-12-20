3d95339dcc1b08bfaa2c8631632dc89a7181ee91
Yuwei Gu <1652423059@qq.com>
2021-12-20 10:32:58 +0800

    Update Flameproof_Screen.ps1

--- Flameproof_Screen.ps1
+++ Flameproof_Screen.ps1
@@ -1,9 +1,14 @@
 #Start-Process powershell -Verb runAs
 
 $shell = New-Object -com "Wscript.Shell"
+$count = 1
 
-while($True)
-{
+while ($True) {
+    $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+    Write-Output (">>> " + $time + "   Press Space " + $count + " Times. ")
+
     Start-Sleep -Seconds 295
     $shell.sendkeys(" ")
+    $count++
 }
