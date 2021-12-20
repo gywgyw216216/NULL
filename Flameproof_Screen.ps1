dcb848b345648b72adb5593d9b0f3f267035b59a
Yuwei Gu <1652423059@qq.com>
2021-12-20 14:28:36 +0800

    Update Flameproof_Screen.ps1

--- Flameproof_Screen.ps1
+++ Flameproof_Screen.ps1
@@ -1,14 +1,29 @@
-#Start-Process powershell -Verb runAs
+# Run as Administrator.
+# Start-Process powershell -Verb runAs
 
-$shell = New-Object -com "Wscript.Shell"
+$shell = New-Object -ComObject "Wscript.Shell"
 $count = 1
 
-while ($True) {
+# Get Screen Resolution.
+# $screen = Get-WmiObject win32_videocontroller
+# $width = $screen.CurrentHorizontalResolution
+# $height = $screen.CurrentVerticalResolution
+
+# Write-Output(">>> Resolution: (" + $width + ", " + $height + ")")
+# Write-Output("--------------------------------------------------------")
+# Write-Output("")
+
+while ($True){
+    $shell.sendkeys("+")
     $time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
-    Write-Output (">>> " + $time + "   Press Space " + $count + " Times. ")
+    Write-Output(">>> " + $time + "   Press [Shift] " + $count + " Times. ")
 
-    Start-Sleep -Seconds 295
-    $shell.sendkeys(" ")
     $count++
+    $sleep = Get-Random -Minimum 0.0 -Maximum 300
+
+    Write-Output(">>> Sleep " + $sleep + " Seconds. ")
+    Write-Output("")
+
+    Start-Sleep $sleep
 }
